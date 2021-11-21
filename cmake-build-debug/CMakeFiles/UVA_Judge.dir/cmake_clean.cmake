6dd1c690b693a46d6f6f9828c74fb6d95fad8982
JhangFengWei <jfengwei1999@gmail.com>
2021-11-21 22:23:05 +0800

    First

---
+++ cmake-build-debug/CMakeFiles/UVA_Judge.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVA_Judge.dir/main.c.obj"
+  "UVA_Judge.exe"
+  "UVA_Judge.exe.manifest"
+  "UVA_Judge.pdb"
+  "libUVA_Judge.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UVA_Judge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    unsigned long int n,m;
+    while(scanf("%ld%ld",&n,&m) == 2){
+
+    }
+    #ifndef ONLINE_JUDGE
+        system("pause");
+    #endif
+    return 0;
+}
